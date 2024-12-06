82710cd944d426a2caeda6771f8893fc67b03581
Mariano <nanomartinezbm@gmail.com>
2024-12-06 13:09:40 -0300

    //

--- index.js
+++ index.js
@@ -25,11 +25,11 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use("/admin", patrimonioRoutes);
 
-// const options = {
-//   key: fs.readFileSync('/opt/psa/var/certificates/scfg0cbqs'),
-//   cert: fs.readFileSync('/opt/psa/var/certificates/scfg0cbqs'),
-//   // ca: fs.readFileSync('/opt/psa/var/certificates/scfqdiDyQ') // si tienes un archivo CA bundle
-// };
+const options = {
+  key: fs.readFileSync('/opt/psa/var/certificates/scfx0vp99'),
+  cert: fs.readFileSync('/opt/psa/var/certificates/scfx0vp99'),
+  // ca: fs.readFileSync('/opt/psa/var/certificates/scfqdiDyQ') // si tienes un archivo CA bundle
+};
 
 
 // https.createServer(options, app).listen(5556, () => {
@@ -37,7 +37,7 @@ app.use("/admin", patrimonioRoutes);
 // });
 
 
-  app.listen(3050, () => {
-    console.log(`server listening on port 3050`);
-  });
+  // app.listen(3050, () => {
+  //   console.log(`server listening on port 3050`);
+  // });
   
